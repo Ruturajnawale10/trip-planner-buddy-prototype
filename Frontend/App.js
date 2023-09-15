f2aeed5df3d5e049f788c72b929538b90bdc1692
Utkarsh <utkarshpant112@gmail.com>
2023-09-14 17:44:32 -0700

    Login and SIgnup Page

--- Frontend/App.js
+++ Frontend/App.js
@@ -1,8 +1,14 @@
 import React, { useState, useEffect } from "react";
 import { View, Text, StyleSheet, ActivityIndicator } from "react-native";
-import MapViewPage from "./components/MapViewPage";
 import { SafeAreaView } from "react-native";
 import { Image } from "react-native";
+import { createAppContainer } from 'react-navigation';
+import { createStackNavigator } from 'react-navigation-stack';
+import { NavigationContainer } from '@react-navigation/native';
+import LoginScreen from "./components/LoginScreen";
+import SignupScreen from "./components/SignupScreen";
+import MapViewPage from "./components/MapViewPage";
+import HomePage from "./components/HomePage/HomePage";
 
 const App = () => {
   const [isLogoLoading, setLogoLoading] = useState(false);
@@ -12,21 +18,10 @@ const App = () => {
   };
 
   return (
-    // <View style={styles.container}>
-    <SafeAreaView style={styles.container}>
-      {isLogoLoading ? (
-        <></>
-      ) : (
-        <Image
-          source={require("./assets/logo.png")}
-          style={{ alignSelf: "center", marginTop: 20 }}
-        />
-      )}
-
-      <MapViewPage logoLoaded={logoLoaded} />
-    </SafeAreaView>
-    // {/* <MapScreen /> */}
-    // </View>
+        <NavigationContainer>
+          <AppNavigator />
+        </NavigationContainer>
+   
   );
 };
 
@@ -37,5 +32,27 @@ const styles = StyleSheet.create({
     backgroundColor: "#E9E3E4",
   },
 });
+
+const LoginSignupStack = createStackNavigator(
+  {
+    Login: LoginScreen,
+    Signup: SignupScreen,
+  },
+  {
+    initialRouteName: 'Login', // You can change this to 'Signup' if you want to start with signup
+  }
+);
+
+const AppNavigator = createAppContainer(createStackNavigator(
+  {
+    LoginSignup: LoginSignupStack,
+    MapView: MapViewPage,
+    HomePage: HomePage
+  },
+  {
+    initialRouteName: 'LoginSignup', // Starts with the login/signup screens
+    headerMode: 'none', // Hide the navigation header
+  }
+));
 
 export default App;
