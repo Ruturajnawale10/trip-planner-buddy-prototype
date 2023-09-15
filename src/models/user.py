ea86e3f148b9eef7488b359e69aa79ba032dface
ruturajnawale10 <ruturajnawale99@gmail.com>
2023-09-14 19:35:45 -0700

    - Added sign in and signup API - Redesigned User Model

--- src/models/user.py
+++ src/models/user.py
@@ -1,7 +1,20 @@
 from pymodm import MongoModel, fields
+from pymongo import IndexModel, ASCENDING
 
 class User(MongoModel):
-    email = fields.EmailField(required=True)
-    first_name = fields.CharField(required=True)
-    last_name = fields.CharField(required=False)
-    password = fields.CharField(required=False)
+    username = fields.CharField(required=True)
+    password = fields.CharField(required=True)
+    google_auth = fields.BooleanField(required=False, default=False)
+    profile = fields.EmbeddedDocumentField('Profile', required=False)
+    upcoming_trips = fields.ListField(field=fields.ObjectIdField(), required=False)
+    past_trips = fields.ListField(field=fields.ObjectIdField(), required=False)
+    shared_itineraries = fields.ListField(field=fields.ObjectIdField(), required=False)
+    # Other user-related data
+
+    class Profile(MongoModel):
+        first_name = fields.CharField(required=False)
+        last_name = fields.CharField(required=False)
+        # Other user profile information
+
+    class Meta:
+        indexes = [IndexModel([('username', ASCENDING)], unique=True)]
