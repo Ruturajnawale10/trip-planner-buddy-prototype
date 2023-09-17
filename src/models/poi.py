e61f37ccf785e77be1457a8ec0dfc1fa830cd87b
ruturajnawale10 <ruturajnawale99@gmail.com>
2023-09-16 18:34:38 -0700

    - Added google api to fetch coordinates from address in case the openstreetmap api fails - Changed Poi Schema and added City Class which holds list of Pois for faster searching to city by name

--- src/models/poi.py
+++ src/models/poi.py
@@ -1,7 +1,15 @@
 from pymodm import MongoModel, fields
 from pymongo import IndexModel
 
-class Pois(MongoModel):
+class City(MongoModel):
+    city_name = fields.CharField(required = True)
+    pois = fields.ListField(field=fields.EmbeddedDocumentField('Poi'), blank=True)
+
+    class Meta:
+        indexes = [IndexModel([('city_name', 1)], unique=True)]
+
+class Poi(MongoModel):
+    poi_id = fields.IntegerField(blank=True)
     name = fields.CharField(blank=True)
     city = fields.CharField(blank=True)
     address = fields.CharField(blank=True)
@@ -12,24 +20,17 @@ class Pois(MongoModel):
     review = fields.ListField(blank=True)
     price = fields.FloatField(blank=True)
     isFree = fields.BooleanField(blank=True)
-    timeSpent = fields.EmbeddedDocumentField('TimeSpent', blank=True)
+    minMinutesSpent = fields.IntegerField(blank=True)
+    maxMinutesSpent = fields.IntegerField(blank=True)
     description = fields.CharField(blank=True)
     pincode = fields.IntegerField(blank=True)
     images = fields.ListField(fields.CharField(), blank=True)
     website = fields.CharField(blank=True)
     internationalPhoneNumber = fields.CharField(blank=True)
     generatedDescription = fields.CharField(blank=True)
-    lat = fields.FloatField(blank=True)
-    lon = fields.FloatField(blank=True)
-    
 
     class Meta:
-        indexes = [IndexModel([("name", 1), ("city", 1)], unique=True)]
-
-class Address(MongoModel):
-    city = fields.CharField(required=False)
-    country = fields.CharField(required=False)
-    state = fields.CharField(required=False)
+        indexes = [IndexModel([('poi_id', 1)], unique=True)]
 
 class Location(MongoModel):
     latitude = fields.FloatField()
@@ -47,8 +48,3 @@ class OpeningHours(MongoModel):
 class Day(MongoModel):
     startTime = fields.CharField()
     endTime = fields.CharField()
-
-class TimeSpent(MongoModel):
-    avgTime = fields.FloatField()
-    minTime = fields.FloatField()
-    maxTime = fields.FloatField()
