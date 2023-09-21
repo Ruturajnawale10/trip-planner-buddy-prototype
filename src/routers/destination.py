ac3de00fe31364192f63901994f1b753d5f2288c
ruturajnawale10 <ruturajnawale99@gmail.com>
2023-09-21 16:04:21 -0700

    Changed destination api query using collection intead of model

--- src/routers/destination.py
+++ src/routers/destination.py
@@ -1,27 +1,28 @@
 import json
 from fastapi import APIRouter, HTTPException
 from models.poi import City
+from configs.db import db
 import string
 
 router = APIRouter(
     tags=['Destination']
 )
 
+collection_city = db['city']
 
 @router.get("/api/destination/{destination}")
 def places_list(destination: str):
     destination = destination.replace("%", " ")
     destination = string.capwords(destination)
+    print(destination)
 
     try:
         # Find the city by city_name
-        city = City.objects.get({'city_name': destination})
-    except City.DoesNotExist:
+        city = collection_city.find_one({'city_name': destination})
+    except Exception as e:
+        print(e)
         raise HTTPException(status_code=404, detail="City not found")
 
-    # Convert the City object to a dictionary, excluding the _id field
-    city_dict = city.to_son().to_dict()
-    del city_dict['_id']
+    city['_id'] = str(city["_id"])
 
-    # Return the City object as JSON
-    return json.loads(json.dumps(city_dict))
+    return city
