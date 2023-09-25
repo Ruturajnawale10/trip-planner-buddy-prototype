3bb1e77cfef2240c03b8a5758d8f8469e8dc3050
Roshan Chokshi <chokshiroshan@gmail.com>
2023-09-24 23:03:52 -0700

    Minor login screen error fixed

--- Frontend/components/LoginScreen.js
+++ Frontend/components/LoginScreen.js
@@ -1,72 +1,78 @@
 // LoginScreen.js
 import React, { useState } from "react";
-import { View, Text, TextInput,Image, Button, TouchableOpacity,StyleSheet } from "react-native";
-import AsyncStorage from '@react-native-async-storage/async-storage';
+import {
+  View,
+  Text,
+  TextInput,
+  Image,
+  Button,
+  TouchableOpacity,
+  StyleSheet,
+} from "react-native";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 const LoginScreen = ({ navigation }) => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const theme = {
-    primaryBackground: 'gainsboro', // Change this color to your desired shade of orange
+    primaryBackground: "gainsboro", // Change this color to your desired shade of orange
     // Define other theme colors and styles here if needed
   };
-  
 
   const storeData = async (key, value) => {
     try {
       await AsyncStorage.setItem(key, value);
-      console.log('Data stored successfully');
+      console.log("Data stored successfully");
     } catch (error) {
-      console.error('Error storing data:', error);
+      console.error("Error storing data:", error);
     }
   };
-  
+
   const handleLogin = () => {
-    // Static user data for demonstration   
-      // Prepare the request body
-      const requestBody = {
-        username: email, // Assuming 'email' in your state corresponds to the username
-        password: password,
-      };
+    // Static user data for demonstration
+    // Prepare the request body
+    const requestBody = {
+      username: email, // Assuming 'email' in your state corresponds to the username
+      password: password,
+    };
     console.log(requestBody);
-      // Make a POST request to the sign-in API
-       fetch('http://127.0.0.1:8000:8000/signin', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(requestBody),
+    // Make a POST request to the sign-in API
+    fetch("http://127.0.0.1:8000/signin", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(requestBody),
+    })
+      .then((response) => {
+        if (response.status === 200) {
+          // Successful login, parse the response JSON
+          return response.json();
+        } else {
+          // Invalid credentials or other error, show an alert
+          alert("Invalid credentials");
+          throw new Error("Invalid credentials");
+        }
       })
-        .then((response) => {
-          if (response.status === 200) {
-            // Successful login, parse the response JSON
-            return response.json();
-          } else {
-            // Invalid credentials or other error, show an alert
-            alert('Invalid credentials');
-            throw new Error('Invalid credentials');
-          }
-        })
-        .then((data) => {
-          // Assuming the response contains the username
-          const { username } = data;
-          storeData('username', username);
-          // Navigate to the "HomePage" with the username
-          navigation.navigate('HomePage', { userIsLoggedIn: true });
-        })
-        .catch((error) => {
-          console.error('Login failed:', error);
-        });
+      .then((data) => {
+        // Assuming the response contains the username
+        const { username } = data;
+        storeData("username", username);
+        // Navigate to the "HomePage" with the username
+        navigation.navigate("HomePage", { userIsLoggedIn: true });
+      })
+      .catch((error) => {
+        console.error("Login failed:", error);
+      });
   };
 
- 
-
- 
   return (
-    <View style={[styles.container, { backgroundColor: theme.primaryBackground }]}>
-    <View style={styles.imageContainer}>
+    <View
+      style={[styles.container, { backgroundColor: theme.primaryBackground }]}
+    >
+      <View style={styles.imageContainer}>
         <Image
-          source={require('../assets/logo.png')} // Update the path to your logo image
+          source={require("../assets/logo.png")} // Update the path to your logo image
           style={styles.logo}
         />
       </View>
@@ -76,14 +82,14 @@ const LoginScreen = ({ navigation }) => {
           placeholder="Username"
           value={email}
           onChangeText={setEmail}
-          style={[styles.input, { width: '80%' }]} // Increase the width
+          style={[styles.input, { width: "80%" }]} // Increase the width
         />
         <TextInput
           placeholder="Password"
           value={password}
           onChangeText={setPassword}
           secureTextEntry
-          style={[styles.input, { width: '80%' }]} // Increase the width
+          style={[styles.input, { width: "80%" }]} // Increase the width
         />
         <Button
           title="Login"
@@ -114,7 +120,7 @@ const styles = StyleSheet.create({
     backgroundColor: "white",
     padding: 20,
     borderRadius: 10,
-    width: '80%', // Increase the width of the input container
+    width: "80%", // Increase the width of the input container
     marginTop: 20, // Adjust this margin to create a space between the image and the form
   },
   input: {
@@ -125,10 +131,10 @@ const styles = StyleSheet.create({
   },
   loginButton: {
     // Define your custom button styles here
-    backgroundColor: 'orange', // Background color
+    backgroundColor: "orange", // Background color
     borderRadius: 10, // Rounded corners
     padding: 8, // Padding (smaller)
-    width: '60%', // Decrease the width of the button
+    width: "60%", // Decrease the width of the button
     alignSelf: "center", // Center the button horizontally
   },
   logo: {
