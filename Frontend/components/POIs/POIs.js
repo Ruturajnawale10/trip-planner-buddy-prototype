c6ee804f3c969f3394bbfc8a4438b6ac3a1a05db
Roshan Chokshi <chokshiroshan@gmail.com>
2023-11-20 00:40:01 -0800

    Added support for multiple images in POI.

--- Frontend/components/POIs/POIs.js
+++ Frontend/components/POIs/POIs.js
@@ -6,8 +6,10 @@ import {
   TouchableOpacity,
   StyleSheet,
   ScrollView,
+  ActivityIndicator,
 } from "react-native";
 import { SafeAreaView } from "react-native";
+import Swiper from "react-native-swiper";
 import NavigationBar from "../NavigationButton/NavigationBar";
 import { userName } from "../RecoilStore/RecoilStore";
 import { useRecoilState } from "recoil";
@@ -17,6 +19,9 @@ export default function POIs({ navigation }) {
   const { item, addPOI, day, recommended } = navigation.state.params;
   const [recommendedDescription, setRecommendedDescription] = useState(null);
   const [username, setUsername] = useRecoilState(userName);
+  const [images, setImages] = useState([]);
+  const [load, setLoad] = useState(false);
+  let showAdded;
   if (addPOI == null) {
     console.log("addPOI is null");
     showAdded = false;
@@ -62,78 +67,102 @@ export default function POIs({ navigation }) {
     )
       .then((response) => response.json())
       .then((json) => {
-        console.log("output", json);
         setRecommendedDescription(json);
       })
       .catch((error) => console.error(error));
   };
   useEffect(() => {
-    console.log("********", recommended);
-    if (recommended) {
-      fetchRecommendedDescription(item.city, username, item.poi_id);
+    for (let i = 0; i < item.images.length; i++) {
+      fetch(
+        "https://itin-dev.sfo2.cdn.digitaloceanspaces.com/freeImageSmall/" +
+          item.images[i]
+      )
+        .then((response) => {
+          if (response.status === 200) {
+            setImages((images) => [...images, response.url]);
+          }
+          if (recommended) {
+            fetchRecommendedDescription(item.city, username, item.poi_id);
+          }
+          setLoad(true);
+        })
+        .catch((error) => console.error(error));
     }
   }, []);
   return (
     <SafeAreaView>
       <ScrollView>
-        <SafeAreaView style={styles.container}>
-          <Image
-            source={{
-              uri:
-                "https://itin-dev.sfo2.cdn.digitaloceanspaces.com/freeImageSmall/" +
-                item.images[0],
-            }}
-            style={styles.image}
-          />
-          {/* Destination details */}
-          <Text style={styles.title}>{item.name}</Text>
-          <Text style={styles.description}>{item.description}</Text>
-          {recommended && (
-            <View>
-              <Text style={styles.whyText}>Why we recommend this for you:</Text>
+        {load ? (
+          <SafeAreaView style={styles.container}>
+            <Swiper showsButtons={true} style={{ height: 400 }}>
+              {images.map((image) => (
+                <View style={{ alignItems: "center" }}>
+                  <Image style={styles.image} source={{ uri: image }} />
+                </View>
+              ))}
+            </Swiper>
+            {/* Destination details */}
+            <Text style={styles.title}>{item.name}</Text>
+            <Text style={styles.description}>{item.description}</Text>
 
-              <TouchableOpacity style={styles.recommendedDescription}>
-                <Text style={styles.recommendedDescriptionText}>
-                  {recommendedDescription}
+            {recommended && (
+              <View>
+                <Text style={styles.whyText}>
+                  Why we recommend this for you:
                 </Text>
-              </TouchableOpacity>
-            </View>
-          )}
-          <Text style={styles.details}>City: {item.city}</Text>
-          <Text style={styles.details}>Address: {item.address}</Text>
-          <Text style={styles.details}>Rating: {item.rating} / 5</Text>
-          {showAdded && (
-            <TouchableOpacity
-              style={[styles.favouriteButton, { backgroundColor: "blue" }]}
-              onPress={() => addPOI(item.poi_id)}
-            >
-              <Text style={styles.buttonText}>Add to Itinerary</Text>
-            </TouchableOpacity>
-          )}
-          {!showAdded && (
-            <TouchableOpacity
-              style={[styles.favouriteButton, { backgroundColor: "red" }]}
-              onPress={() => {
-                console.log("remove POI", item.poi_id, day);
-                try {
-                  removePOI(item.poi_id, day);
-                } catch (e) {
-                  console.log(e);
-                }
-                navigation.goBack();
-              }}
-            >
-              <Text style={styles.buttonText}>Remove from Itinerary</Text>
-            </TouchableOpacity>
-          )}
 
-          <TouchableOpacity
-            style={styles.backButton}
-            onPress={() => navigation.goBack()}
-          >
-            <Text style={styles.buttonText}>Go Back</Text>
-          </TouchableOpacity>
-        </SafeAreaView>
+                <TouchableOpacity style={styles.recommendedDescription}>
+                  <Text style={styles.recommendedDescriptionText}>
+                    {recommendedDescription}
+                  </Text>
+                </TouchableOpacity>
+              </View>
+            )}
+            <Text style={styles.details}>City: {item.city}</Text>
+            <Text style={styles.details}>Address: {item.address}</Text>
+            <Text style={styles.details}>Rating: {item.rating} / 5</Text>
+            {showAdded && (
+              <TouchableOpacity
+                style={[styles.favouriteButton, { backgroundColor: "blue" }]}
+                onPress={() => {
+                  try {
+                    addPOI(item.poi_id);
+                  } catch (e) {
+                    console.error(e);
+                  }
+                }}
+              >
+                <Text style={styles.buttonText}>Add to Itinerary</Text>
+              </TouchableOpacity>
+            )}
+            {!showAdded && (
+              <TouchableOpacity
+                style={[styles.favouriteButton, { backgroundColor: "red" }]}
+                onPress={() => {
+                  console.log("remove POI", item.poi_id, day);
+                  try {
+                    removePOI(item.poi_id, day);
+                  } catch (e) {
+                    console.log(e);
+                  }
+                  navigation.goBack();
+                }}
+              >
+                <Text style={styles.buttonText}>Remove from Itinerary</Text>
+              </TouchableOpacity>
+            )}
+
+            <TouchableOpacity
+              style={styles.backButton}
+              onPress={() => navigation.goBack()}
+            >
+              <Text style={styles.buttonText}>Go Back</Text>
+            </TouchableOpacity>
+            <View style={{ height: 100 }} />
+          </SafeAreaView>
+        ) : (
+          <ActivityIndicator size="large" color="#0000ff" />
+        )}
       </ScrollView>
 
       <NavigationBar navigation={navigation} />
@@ -145,7 +174,8 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     alignItems: "center",
-    justifyContent: "flex-start", // Adjust the vertical alignment as needed
+    justifyContent: "flex-start",
+    // Adjust the vertical alignment as needed
   },
   image: {
     width: "100%", // Adjust the image width as needed
