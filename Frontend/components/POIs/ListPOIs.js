594b18889e26f484b23b2a12779e27cf810b37b7
Roshan Chokshi <chokshiroshan@gmail.com>
2023-11-30 17:16:18 -0800

    Added Tag Selector.

--- Frontend/components/POIs/ListPOIs.js
+++ Frontend/components/POIs/ListPOIs.js
@@ -4,6 +4,7 @@ import { View, StyleSheet, ScrollView, Text, Dimensions } from "react-native";
 
 import { SafeAreaView } from "react-native";
 import POIsCard from "./POIsCard";
+import { TouchableOpacity } from "react-native-gesture-handler";
 
 const ListPOIs = ({
   navigation,
@@ -18,6 +19,8 @@ const ListPOIs = ({
 }) => {
   const { location, startDate, endDate, trip_id } = navigation.state.params;
   const [load, setLoad] = useState(false);
+  const [tags, setTags] = useState(new Set());
+  const [currentTag, setCurrentTag] = useState("");
   const onPress = () => {
     console.log("pressed");
   };
@@ -29,6 +32,10 @@ const ListPOIs = ({
       } else {
         item.recommended = false;
       }
+      item.show = true;
+      item.type.map((tag) => {
+        tags.add(tag);
+      });
     });
     data.pois.sort((a, b) => {
       return b.recommended - a.recommended;
@@ -37,12 +44,54 @@ const ListPOIs = ({
     setLoad(true);
   };
 
+  const onTagPress = (tag) => () => {
+    setCurrentTag(tag);
+    if (tag === "All" || tag === currentTag) {
+      data.pois.map((item) => {
+        item.show = true;
+      });
+      return;
+    }
+    data.pois.map((item) => {
+      if (item.type.includes(tag)) {
+        item.show = true;
+      } else {
+        item.show = false;
+      }
+    });
+  };
+
   useEffect(() => {
     integrateRecommendations();
   }, [data]);
 
   return (
     <SafeAreaView style={styles.container}>
+      <ScrollView horizontal={true}>
+        <TouchableOpacity
+          key={"All" + day}
+          style={currentTag === "All" ? styles.tagSelected : styles.tag}
+          onPress={onTagPress("All")}
+        >
+          <Text>{"All"}</Text>
+        </TouchableOpacity>
+        {Array.from(tags).map((tag) => (
+          <TouchableOpacity
+            key={tag + day}
+            style={currentTag === tag ? styles.tagSelected : styles.tag}
+            onPress={onTagPress(tag)}
+          >
+            <Text
+              style={
+                currentTag === tag ? { color: "white" } : { color: "#412a47" }
+              }
+            >
+              {tag}
+            </Text>
+          </TouchableOpacity>
+        ))}
+      </ScrollView>
+
       {load ? (
         <ScrollView
           horizontal={true}
@@ -52,22 +101,25 @@ const ListPOIs = ({
           }}
           scrollEventThrottle={16}
         >
-          {data.pois.map((item) => (
-            <View key={item.poi_id} style={styles.poiCard}>
-              <POIsCard
-                imageID={item.images[0]}
-                poi_name={item.name}
-                poi_id={item.poi_id}
-                recommended={item.recommended}
-                navigation={navigation}
-                addPOI={addPOI}
-                removePOI={removePOI}
-                item={item}
-                onPress={onPress}
-                day={day}
-              />
-            </View>
-          ))}
+          {data.pois.map(
+            (item) =>
+              item.show && (
+                <View key={item.poi_id} style={styles.poiCard}>
+                  <POIsCard
+                    imageID={item.images[0]}
+                    poi_name={item.name}
+                    poi_id={item.poi_id}
+                    recommended={item.recommended}
+                    navigation={navigation}
+                    addPOI={addPOI}
+                    removePOI={removePOI}
+                    item={item}
+                    onPress={onPress}
+                    day={day}
+                  />
+                </View>
+              )
+          )}
         </ScrollView>
       ) : (
         <Text>Loading...</Text>
@@ -91,6 +143,24 @@ const styles = StyleSheet.create({
     margin: 10,
     display: "flex",
   },
+  tag: {
+    backgroundColor: "#E9E3E4",
+    borderRadius: 10,
+    padding: 10,
+    margin: 10,
+    alignItems: "center",
+  },
+  tagSelected: {
+    backgroundColor: "#412a47",
+
+    borderRadius: 10,
+    padding: 10,
+    margin: 10,
+    alignItems: "center",
+    borderWidth: 2,
+    borderColor: "#ffffff",
+  },
+
   text: {
     fontSize: 20,
     textAlign: "left",
